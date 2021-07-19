49eb2d5146dd713f06204a515ddb365f2605cc18
Tom Barber <trb5160@gmail.com>
2021-07-19 00:53:56 -0400

    added env file and fixed seeding issues

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,8 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
+      username: 'root',
+      password: 'Witcher91@wh3',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
